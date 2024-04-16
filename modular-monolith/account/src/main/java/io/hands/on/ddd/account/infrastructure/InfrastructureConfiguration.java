f53ed6e9bfa02caec26ee7f893bbca4c6a5355c1
Piotr <pmarat.dev@gmail.com>
2024-04-16 21:27:42 +0200

    Added infrastructure configuration.

---
+++ modular-monolith/account/src/main/java/io/hands/on/ddd/account/infrastructure/InfrastructureConfiguration.java
@@ -0,0 +1,20 @@
+package io.hands.on.ddd.account.infrastructure;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static io.hands.on.ddd.account.AccountUserConfiguration.MODULE_PREFIX;
+import static io.hands.on.ddd.account.infrastructure.InfrastructureConfiguration.BeanNames.*;
+
+/**
+ * Spring configuration for infrastructure layer in the module.
+ */
+@Configuration(value = INFRASTRUCTURE_CONFIGURATION, proxyBeanMethods = false)
+public class InfrastructureConfiguration {
+    public static class BeanNames {
+        public static final String INFRASTRUCTURE_CONFIGURATION = MODULE_PREFIX + "InfrastructureConfiguration";
+        public static final String ACCOUNT_REPOSITORY = MODULE_PREFIX + "AccountRepository";
+    }
+}
