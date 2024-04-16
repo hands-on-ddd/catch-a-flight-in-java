34371fb4077cb6eb33189572aa17a3e2b4f19586
Piotr <pmarat.dev@gmail.com>
2024-04-16 21:27:57 +0200

    Added main configuration.

---
+++ modular-monolith/account/src/main/java/io/hands/on/ddd/account/AccountUserConfiguration.java
@@ -0,0 +1,19 @@
+package io.hands.on.ddd.account;
+
+import io.hands.on.ddd.account.application.ApplicationConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Main spring configuration aggregator for the module.
+ */
+@Configuration(proxyBeanMethods = false)
+@ComponentScan(AccountUserConfiguration.PACKAGE_TO_SCAN)
+@Import({
+      ApplicationConfiguration.class,
+})
+public class AccountUserConfiguration {
+    public static final String MODULE_PREFIX = "account";
+    public static final String PACKAGE_TO_SCAN = "io.pmarat.catchflight.account";
+}
