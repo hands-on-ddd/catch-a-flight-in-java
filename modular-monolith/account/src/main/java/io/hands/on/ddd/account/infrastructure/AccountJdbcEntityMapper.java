216a7d3254138f390329fdaf21f3d9b46dcb30e9
Piotr <pmarat.dev@gmail.com>
2024-04-17 19:24:48 +0200

    Added account infrastructure classes.

---
+++ modular-monolith/account/src/main/java/io/hands/on/ddd/account/infrastructure/AccountJdbcEntityMapper.java
@@ -0,0 +1,53 @@
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Copyright (c) 2024 Piotr Marat
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+package io.hands.on.ddd.account.infrastructure;
+
+import io.hands.on.ddd.account.model.Account;
+import io.hands.on.ddd.account.model.Password;
+import io.hands.on.ddd.account.model.UserName;
+import io.hands.on.hands.sharedkernel.Email;
+import io.hands.on.hands.sharedkernel.UserId;
+import io.pmarat.catchflight.common.persistence.Version;
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Implementation
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Maps account domain model to/from entity.
+ */
+class AccountJdbcEntityMapper {
+    AccountJdbcEntity toJdbcEntity(Account account) {
+        var userId = account.getUserId();
+        var userName = account.getUserName();
+        var email = account.getEmail();
+        var password = account.getPassword();
+        var userType = account.getAccountType();
+
+        return new AccountJdbcEntity(
+              userId != null ? userId.value() : null,
+              email.value(),
+              password.value(),
+              userName.firstName(),
+              userName.lastName(),
+              userType);
+    }
+
+    Account toDomain(AccountJdbcEntity entity) {
+        var userId = new UserId(entity.id());
+        var userName = new UserName(entity.firstName(), entity.lastName());
+        var email = new Email(entity.email());
+        var password = new Password(entity.password());
+        var userType = entity.accountType();
+
+        return Account.builder()
+              .userId(userId)
+              .userName(userName)
+              .email(email)
+              .password(password)
+              .accountType(userType)
+              .build();
+    }
+}
--- modular-monolith/account/src/main/java/io/hands/on/ddd/account/infrastructure/InfrastructureConfiguration.java
+++ modular-monolith/account/src/main/java/io/hands/on/ddd/account/infrastructure/InfrastructureConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 import static io.hands.on.ddd.account.AccountUserConfiguration.MODULE_PREFIX;
 import static io.hands.on.ddd.account.infrastructure.InfrastructureConfiguration.BeanNames.*;
@@ -19,10 +20,26 @@ import static io.hands.on.ddd.account.infrastructure.InfrastructureConfiguration
 /**
  * Spring configuration for infrastructure layer in the module.
  */
+@EntityScan
+@EnableJdbcRepositories
 @Configuration(value = INFRASTRUCTURE_CONFIGURATION, proxyBeanMethods = false)
 public class InfrastructureConfiguration {
+    @Bean(ACCOUNT_JDBC_ENTITY_MAPPER)
+    AccountJdbcEntityMapper userJdbcEntityMapper() {
+        return new AccountJdbcEntityMapper();
+    }
+
+    @Bean(ACCOUNT_REPOSITORY)
+    AccountRepository userRepository(
+          @Qualifier(ACCOUNT_JDBC_REPOSITORY) AccountJdbcRepository repository,
+          @Qualifier(ACCOUNT_JDBC_ENTITY_MAPPER) AccountJdbcEntityMapper mapper) {
+        return new AccountRepository(repository, mapper);
+    }
+
     public static class BeanNames {
         public static final String INFRASTRUCTURE_CONFIGURATION = MODULE_PREFIX + "InfrastructureConfiguration";
+        public static final String ACCOUNT_JDBC_REPOSITORY = MODULE_PREFIX + "AccountJdbcRepository";
         public static final String ACCOUNT_REPOSITORY = MODULE_PREFIX + "AccountRepository";
+        public static final String ACCOUNT_JDBC_ENTITY_MAPPER = MODULE_PREFIX + "AccountJdbcEntityMapper";
     }
 }
