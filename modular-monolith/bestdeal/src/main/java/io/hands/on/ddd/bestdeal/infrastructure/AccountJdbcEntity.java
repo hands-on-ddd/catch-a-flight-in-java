94dbd692990bc4a26a8e5c4c2326efb3baedc433
Piotr <pmarat.dev@gmail.com>
2024-04-21 15:33:22 +0200

    Added best infrastructure classes.

---
+++ modular-monolith/bestdeal/src/main/java/io/hands/on/ddd/bestdeal/infrastructure/AccountJdbcEntity.java
@@ -0,0 +1,35 @@
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Copyright (c) 2024 Piotr Marat
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+package io.hands.on.ddd.bestdeal.infrastructure;
+
+import io.hands.on.hands.sharedkernel.AccountType;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.UUID;
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Implementation
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Account entity (Spring Data JDBC).
+ * @param id          primary key
+ * @param email       email provided at registration
+ * @param password    encrypted password
+ * @param firstName   user's first name
+ * @param lastName    user's last name
+ * @param accountType type of the user (Regular or Premium, at the moment)
+ */
+@Table(name = "ACCOUNTS")
+public record AccountJdbcEntity(
+      @Id
+      UUID id,
+      String email,
+      String password,
+      String firstName,
+      String lastName,
+      AccountType accountType) {
+}
