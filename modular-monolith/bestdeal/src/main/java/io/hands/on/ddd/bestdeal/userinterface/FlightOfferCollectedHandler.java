4fc0876792e17a01dae516907c9b9bef1782f02e
Piotr <pmarat.dev@gmail.com>
2024-05-05 02:02:44 +0200

    Added bestdeal user-interface classes.

---
+++ modular-monolith/bestdeal/src/main/java/io/hands/on/ddd/bestdeal/userinterface/FlightOfferCollectedHandler.java
@@ -0,0 +1,30 @@
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Copyright (c) 2024 Piotr Marat
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+package io.hands.on.ddd.bestdeal.userinterface;
+
+import io.hands.on.ddd.bestdeal.application.IndexFlightQueryUseCase;
+import io.pmarat.catchflight.common.annotations.hexagonal.MessageListenerAdapter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Implementation
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/**
+ * FlightOfferCollected event handler.
+ */
+@Slf4j
+@MessageListenerAdapter
+@RequiredArgsConstructor
+class FlightOfferCollectedHandler {
+    private final IndexFlightQueryUseCase indexFlightQueryUseCase;
+
+//    @EventListener
+//    void handleEvent(FlightOfferCollected flightOfferCollected) {
+//        log.info("Event consumed. Event Id: {}", flightOfferCollected.eventId());
+//    }
+}
---
+++ modular-monolith/bestdeal/src/main/java/io/hands/on/ddd/bestdeal/userinterface/package-info.java
@@ -0,0 +1,6 @@
+@NonNullApi
+@NonNullFields
+package io.hands.on.ddd.bestdeal.userinterface;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
